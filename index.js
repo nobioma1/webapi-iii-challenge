4b83dc7bb591e2c8ee9244d13bbef89ea6ccebb0
Noble Obioma <nobioma1@gmail.com>
2019-07-10 11:33:57 +0100

    Install dependencies and setup express application

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const server = require('./server');
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () =>
+  console.log(`Server live on http://localhost:${PORT}`),
+);
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
 
 const server = express();
 
+server.use(express.json());
+server.use(helmet());
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Welcome Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
