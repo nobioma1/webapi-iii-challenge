a85499a8d13dafc099429c3a4d1cb8227377152f
Noble Obioma <nobioma1@gmail.com>
2019-07-10 11:48:34 +0100

    Create Logger middleware

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log(`Server live on http://localhost:${PORT}`),
 );
--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(logger);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Welcome Let's write some middleware!</h2>`);
+  res.send(`<h2>Welcome, Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const { method, url } = req;
+  const timestamp = new Date().toISOString();
+  console.log(method, url, timestamp);
+  next();
+}
 
 module.exports = server;
