292eecc4e85cc31359579456c2ec640b718c1011
Noble Obioma <nobioma1@gmail.com>
2019-07-11 12:19:37 +0100

    Move client to new branch and move server to root

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import userReducer from './userReducer';
-import postReducer from './postReducer';
-
-export default combineReducers({
-  userReducer,
-  postReducer,
-});
